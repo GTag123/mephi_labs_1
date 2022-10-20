a25a7c868a22c11588752954af46a62c4e07a75f
Tagir <gtagir11@yandex.ru>
2022-10-20 17:20:01 +0300

    test

--- containers/vector/array.h
+++ containers/vector/array.h
@@ -1,11 +1,45 @@
 #include <iostream>
+#include "ostream"
+#include "string"
 
 class Array {
 public:
-    Array(std::ostream& ostream);
-    Array(const Array& array);
-    Array(size_t size, std::ostream& ostream = std::cout, int defaultValue = 0);
-    ~Array();
+    std::ostream& Ostream_;
+    std::string getStr(){
+        return "Result Array's capacity is " + std::to_string(Capacity_) + "and size is " + std::to_string(Size_);
+    }
+    Array(std::ostream& ostream):
+            Ostream_(ostream){
+        Capacity_ = 2;
+        Size_ = 0;
+        Elements_ = new int[Capacity_];
+        Ostream_ << "Constructed. " << getStr() << std::endl;
+    };
+    Array(const Array& array)
+    : Size_(array.Size()),
+      Capacity_(array.Capacity()),
+      Ostream_(array.Ostream_){
+        Elements_ = new int[Capacity_];
+        for (int i = 0; i < Size_; ++i) {
+            Elements_[i] = array[i];
+        }
+        Ostream_ << "Constructed from another Array. " << getStr() << std::endl;
+    };
+
+    Array(size_t size, std::ostream& ostream = std::cout, int defaultValue = 0)
+    : Ostream_(ostream){
+        Size_ = size;
+        Capacity_ = size * 2;
+        Elements_ = new int[Capacity_];
+        for (int i = 0; i < size; ++i) {
+            Elements_[i] = defaultValue;
+        }
+        Ostream_ << "Constructed with default. " << getStr() << std::endl;
+
+    };
+    ~Array() {
+        std::cout << "Destructed " << Size() << std::endl; // add size
+    };
 
     size_t Size() const;
     size_t Capacity() const;
@@ -34,5 +68,5 @@ private:
     int* Elements_;
     size_t Size_;
     size_t Capacity_;
-    std::ostream& Ostream_;
+
 };
