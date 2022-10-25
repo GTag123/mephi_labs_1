f0a6dc70e00fa4a540a565d3282e9ef0a10c9713
Tagir <gtagir11@yandex.ru>
2022-10-26 01:00:10 +0300

    range

--- iterators/range/main.cpp
+++ iterators/range/main.cpp
@@ -6,6 +6,7 @@
 
 #include <cassert>
 #include <vector>
+#include <cstdint>
 
 namespace {
 
@@ -27,22 +28,37 @@ namespace {
 }
 
 bool operator==(const Range& range, const std::vector<int64_t>& vec) {
+    std::cout << range.Size() << " " << vec.size() << std::endl;
     if (range.Size() != static_cast<int64_t>(vec.size())) {
+        std::cout << "false" << std::endl;
         return false;
     }
 
     auto range_iter = range.begin();
     auto vec_iter = vec.begin();
+
     while (range.end() != range_iter && vec.end() != vec_iter) {
+//        std::cout << "RI: " << *range_iter << std::endl;
+//        std::cout << "VI: " << *vec_iter << std::endl;
         if (*vec_iter++ != *range_iter++) {
+            std::cout << "false2" << std::endl;
             return false;
         }
     }
-
+//    std::cout << "RI: " << *range_iter << std::endl;
+//    std::cout << "VI: " << *vec_iter << std::endl;
+//    std::cout << std::endl;
+    if (!(range_iter == range.end() && vec_iter == vec.end())){
+        std::cout << "errRI: " << *range_iter << "  r.end(): " << *(range.end()) << std::endl;
+        std::cout << "errVI: " << *vec_iter << "  v.end(): " << *(vec.end()) << std::endl;
+    }
     return range_iter == range.end() && vec_iter == vec.end();
 }
 
 void TestProperties() {
+//    std::cout << sizeof(Range) << std::endl;
+//    std::cout << sizeof(Range(123456790)) << std::endl;
+//    std::cout << sizeof(Range(123456790)) << std::endl;
     assert(sizeof(Range) < 25);
     assert(sizeof(Range(123456790).begin()) < 25);
     assert(sizeof(Range(123456790).end()) < 25);
@@ -65,32 +81,43 @@ void TestSimple() {
 
     assert(Range(-100, 99, 7) == GenerateSequence(-100, 99, 7));
     assert(Range(99, -100, -5) == GenerateSequence(99, -100, -5));
+    std::cout << 83 << std::endl;
     assert(Range(100, -100, -111) == GenerateSequence(100, -100, -111));
+    std::cout << 84 << std::endl;
     assert(Range(-100, 100, 200) == GenerateSequence(-100, 100, 200));
     assert(Range(-100, 100, 201) == GenerateSequence(-100, 100, 201));
-
+    std::cout << 88 << std::endl;
     assert(Range(-5, -5, 3) == GenerateSequence(-5, -5, 3));
     assert(Range(-123, -12, 10) == GenerateSequence(-123, -12, 10));
+    std::cout << 91 << std::endl;
     assert(Range(-12, -123, -10) == GenerateSequence(-12, -123, -10));
+
     assert(Range(-100, -1, 531) == GenerateSequence(-100, -1, 531));
+    std::cout << 95 << std::endl;
 }
 
 void TestBig() {
+    std::cout << "big" << std::endl;
     const int64_t kMaxN = 0xdead0beefLL;
-
+    std::cout << "big11" << std::endl;
     const Range big_range(kMaxN);
+    std::cout << "big111" << std::endl;
     assert(big_range.Size() == kMaxN);
-
+    std::cout << "big12" << std::endl;
     auto big_range_iter = big_range.begin();
+    std::cout << "big13" << std::endl;
     for (int i = 0; i < 1000000; ++i) {
-        assert(*big_range_iter++ == i);
+        assert(*(big_range_iter++) == i);
     }
-
+    std::cout << "big2" << std::endl;
     const Range large_range(kMaxN, -kMaxN, -1357);
+    std::cout << "big3" << std::endl;
     auto large_range_iter = large_range.begin();
+    std::cout << "big4" << std::endl;
     for (int i = 0; i < 1000000; ++i) {
         assert(*large_range_iter++ == kMaxN - i * 1357LL);
     }
+    std::cout << "big5" << std::endl;
 }
 
 int main() {
--- iterators/range/range.h
+++ iterators/range/range.h
@@ -1,5 +1,90 @@
 #pragma once
+#include "iostream"
+#include "vector"
+class RIter {
+private:
+    int64_t stp_, curr_, end_;
+public:
+    RIter(int64_t step, int64_t current, int64_t end): stp_(step), curr_(current), end_(end){
+    };
+    int64_t operator *() const{
+        return this->curr_;
+    }
+    RIter& operator++(){
+
+        if(stp_ > 0 && this->curr_ + stp_ > end_){
+            this->curr_ = this->end_;
+            return *this;
+        }
+        else if(stp_ < 0 && this->curr_ + stp_ < end_){
+            this->curr_ = this->end_;
+            return *this;
+        }
+        this->curr_ += stp_;
+        return *this;
+    }
+    RIter operator++(int){
+        RIter old = *this;
+        operator++();
+        return old;
+    }
+
+    friend RIter& operator+(int64_t rhs, RIter& lhs){
+        lhs.curr_ += rhs*lhs.stp_;
+        return lhs;
+    }
+    friend bool operator!=(const RIter& it1, const RIter& it2){
+        if (it1.curr_ == it2.curr_ && it1.stp_ == it2.stp_){
+
+            return false;
+        }
+        return true;
+    };
+    friend bool operator==(const RIter& it1, const RIter& it2){
+        if (it1 != it2) return false;
+        return true;
+    }
+
+};
 
 class Range {
-    // Your code here
+private:
+    const long long int start_;
+    const long long int end_;
+    const long long int step_;
+public:
+    explicit Range(long long int n):
+        start_(0),
+        end_(n),
+        step_(1){};
+    Range(long long int start, long long int end):
+        start_(start),
+        end_(end),
+        step_(1){};
+    Range(long long int start, long long int end, long long int step):
+            start_(start),
+            end_(end),
+            step_(step){};
+
+    [[nodiscard]] const RIter begin() const {
+        return RIter(step_, start_, end_);
+    }
+    [[nodiscard]] const RIter end() const {
+        return RIter(step_, end_, end_);
+    }
+
+    [[nodiscard]] int64_t Size() const{
+        if (!(std::abs(start_ - end_) % std::abs(step_))){
+            return std::abs(start_ - end_) / std::abs(step_);
+        }
+        return std::abs(start_ - end_) / std::abs(step_) + 1;
+    }
+//    friend bool operator ==(Range& it1, std::vector<int64_t>& it2){
+//        std::cout << "st1 " << it1.start_ << ", end1: " << it1.end_ << ", step1: " << it1.step_
+//        << std::endl;
+//        std::cout << "it2[0] " << it2[0] << ", it2[1]: " << it2[1] << ", it2[1]-[0]: " << it2[1] - it2[0]
+//                  << "ending: " << it2[it2.size()-1] << std::endl << std::endl;
+//        return true;
+//    }
+
 };
