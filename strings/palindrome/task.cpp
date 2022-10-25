52c8006a23d0863293824f52bc7f6064720d3b08
Tagir <gtagir11@yandex.ru>
2022-10-26 01:40:48 +0300

    palindrom

--- strings/palindrome/task.cpp
+++ strings/palindrome/task.cpp
@@ -1,8 +1,9 @@
 #include "task.h"
 bool is_palindrome(const std::string& s) {
-    std::string st = s;
-    st.erase(std::remove(st.begin(), st.end(), '?'), st.end());
-    st.erase(std::remove(st.begin(), st.end(), ' '), st.end());
+    std::string st;
+    for (int i = 0; i < (int)s.size(); ++i) {
+        if (s[i] != ' ' && s[i] != '?') st += s[i];
+    }
     for (int i = st.size()-1; i >= 0; --i) {
         if (tolower(st[st.size()-1-i]) != tolower(st[i])){
             return false;
