dd25b1ed299cfaff71f6300be03295c1bd56bdd9
Tagir <gtagir11@yandex.ru>
2022-10-26 17:51:35 +0300

    heap

--- advanced_containers/heap_sort/task.cpp
+++ advanced_containers/heap_sort/task.cpp
@@ -3,4 +3,14 @@
 using namespace std;
 
 vector<int> HeapSort(const vector<int>& v) {
+    priority_queue<int> m;
+    for (int i: v){
+        m.push(i);
+    }
+    vector<int> f;
+    for (int i = 0; i < (int)m.size(); ++i) {
+        f.push_back(m.top());
+        m.pop();
+    }
+    return f;
 }
