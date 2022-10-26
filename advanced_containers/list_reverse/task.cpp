f3f8976ed4c991b45d9cf75d322d63a8867d8085
Tagir <gtagir11@yandex.ru>
2022-10-26 14:39:10 +0300

    list_reverse

--- advanced_containers/list_reverse/task.cpp
+++ advanced_containers/list_reverse/task.cpp
@@ -1,4 +1,11 @@
 #include "task.h"
 
 void ReverseList(std::list<int>& l) {
+    auto b = l.rbegin();
+    std::list<int> l2;
+    while (b != l.rend()){
+        l2.push_back(*b++);
+    }
+    l = l2;
+
 }
